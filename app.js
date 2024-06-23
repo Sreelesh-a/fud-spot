c7177f5161de401d74d265d722447c22f49e19bf
sreelesh <mail.sreelesh@gmai.com>
2024-06-23 13:17:33 +0530

    connect ReactDOM root Start Project

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import logoHeaderImg from "./img/fud-spot-log.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logoHeader">
+        <img src={logoHeaderImg} className="logoHeader"></img>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="AppLayout">
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
