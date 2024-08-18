b6ed3b8fcab70e443ba1a9b2119d27e268c092cf
sreelesh <mail.sreelesh@gmai.com>
2024-08-18 11:32:43 +0530

    testing

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -5,11 +5,11 @@ const Offers = () => {
       <div className="text-center mt-32">
         <h1>Offer Closes Soon...</h1>
         <div className="text-2xl font-bold">New Offers</div>
-        <div className="flex justify-center gap-6 p-6">
-          <div className="w-44 h-32 animate-pulse bg-gray-200 rounded-xl"></div>
-          <div className="w-44 h-32 animate-pulse bg-gray-200 rounded-xl"></div>
-          <div className="w-44 h-32 animate-pulse bg-gray-200 rounded-xl"></div>
-          <div className="w-44 h-32 animate-pulse bg-gray-200 rounded-xl"></div>
+        <div className="flex justify-center gap-6 p-6 animate-pulse">
+          <div className="w-44 h-32  bg-gray-200 rounded-xl"></div>
+          <div className="w-44 h-32  bg-gray-200 rounded-xl"></div>
+          <div className="w-44 h-32  bg-gray-200 rounded-xl"></div>
+          <div className="w-44 h-32  bg-gray-200 rounded-xl"></div>
         </div>
       </div>
     </>
